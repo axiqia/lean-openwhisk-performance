170f4582f943ed76cdc492b5e48b95d1b112c8a7
axiqia <axiqia@qq.com>
2019-04-03 17:13:37 +0800

    add latency with 1Gb and 2GB

--- repeats.sh
+++ repeats.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 tmp=tmpfile
 function runload {
-    /usr/local/bin/loadtest --insecure -n 50 -c 1 -k -m POST -H "authorization: Basic MjNiYzQ2YjEtNzFmNi00ZWQ1LThjNTQtODE2YWE0ZjhjNTAyOjEyM3pPM3haQ0xyTU42djJCS0sxZFhZRnBYbFBrY2NPRnFtMTJDZEFzTWdSVTRWck5aOWx5R1ZDR3VNREdJd1A=" -T "application/json" -p "timeout30" "https://172.17.0.1/api/v1/namespaces/guest/actions/sleepy?blocking=true">$tmp
+    /usr/local/bin/loadtest --insecure -n 100  -c 1 -k -m POST -H "authorization: Basic MjNiYzQ2YjEtNzFmNi00ZWQ1LThjNTQtODE2YWE0ZjhjNTAyOjEyM3pPM3haQ0xyTU42djJCS0sxZFhZRnBYbFBrY2NPRnFtMTJDZEFzTWdSVTRWck5aOWx5R1ZDR3VNREdJd1A=" -T "application/json" -p "timeout30" "https://172.17.0.1/api/v1/namespaces/guest/actions/sleepy?blocking=true">$tmp
     cat $tmp
     cat $tmp >> alllog
 }
@@ -17,8 +17,9 @@ done
 }
 for mem in `seq 1 1 5`
 do 
-    echo "memory"=$mem*2048 >> alllog
-    wskdev -d invoker -t local -e invoker_user_memory="$[$mem*2048]m" -e skip_pull_runtimes=True
+    date >> alllog
+    echo "memory"=$mem*1024 >> alllog
+    wskdev -d invoker -t local -e invoker_user_memory="$[$mem*1024]m" -e skip_pull_runtimes=True
     sleep 20
     repeat
 done 
